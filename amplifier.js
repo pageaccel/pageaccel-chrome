ca33662815672de04c79a0ca84d80297507ceb48
Taylor Raack <taylor@raack.info>
2016-11-09 18:09:58 -0800

    Update to have persistent state on each tab

--- amplifier.js
+++ amplifier.js
@@ -1,3 +1,21 @@
+/*
+(C) Portions copyright 2016 Taylor Raack.
+Some portions from public domain
+
+Amplifier is free software: you can redistribute it and/or modify
+it under the terms of the Affero GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Amplifier is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Affero GNU General Public License for more details.
+
+You should have received a copy of the Affero GNU General Public License
+along with Amplifier.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 (function() {
     var amplink = document.querySelector("link[rel='amphtml']");
     var canonical = document.querySelector("link[rel='canonical']");
@@ -6,6 +24,7 @@
         sentinel: "__AMPLIFIER__",
         ampurl : null,
         canonical : null,
+        amplifierOn: true,
         isamp : (document.querySelector("html[amp]") !== null ||  document.querySelector("html[⚡]") !== null)
     };
 
@@ -17,7 +36,7 @@
         amp.canonical = canonical.href;
     }
    
-    
+    // send information about the current page to the processor
     chrome.runtime.sendMessage(amp);
 
 })();
--- background.js
+++ background.js
@@ -1,36 +1,79 @@
+/*
+(C) Portions copyright 2016 Taylor Raack.
+Some portions from public domain
+
+Amplifier is free software: you can redistribute it and/or modify
+it under the terms of the Affero GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Amplifier is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Affero GNU General Public License for more details.
+
+You should have received a copy of the Affero GNU General Public License
+along with Amplifier.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// state of all amp on/offs in the pages
 var ampTabs = [];
 
 
+// excuted when amplified.js sends message
 chrome.runtime.onMessage.addListener(function(amp, sender, sendResponse) {
     if (amp.sentinel === undefined || amp.sentinel != "__AMPLIFIER__") {
       return;  //not from amplifier
     }
     if (amp.isamp) {
+      // we are currently viewing an amp page
       chrome.pageAction.setIcon({tabId:sender.tab.id, path: 'canonical.png'});
       chrome.pageAction.setTitle({tabId:sender.tab.id, title: 'Show the Canonical version of this page'});
     } else {
+      // we are not currently viewing an amp page
+    	
       chrome.pageAction.setIcon({tabId:sender.tab.id, path: 'amplify.png'});
       chrome.pageAction.setTitle({tabId:sender.tab.id, title: 'Show the AMP version of this page'});
     }
     if(amp.ampurl !== null || (amp.canonical !=null && amp.isamp)) {
+      // there is an amp url or (there is a canonical url and we're currently on an amp version)
       chrome.pageAction.show(sender.tab.id);
     }
-    ampTabs[sender.tab.id] = amp;
+    // record the page state
+    if (sender.tab.id in ampTabs) {
+    	ampTabs[sender.tab.id].ampurl = amp.ampurl;
+    	ampTabs[sender.tab.id].canonical = amp.canonical;
+    	ampTabs[sender.tab.id].isamp = amp.isamp;
+    	
+    } else {
+    	ampTabs[sender.tab.id] = amp;
+    }
+    
     
     if (localStorage["devMode"] == "true") {
       amp.ampurl += "#development=1";
     }
-   
+    
+    // if amplifer is turned on for this tab, and there is an amp page, and we're not on it, load it up
+    if (ampTabs[sender.tab.id].amplifierOn && !amp.isamp && amp.ampurl != null) {
+    	chrome.tabs.update(sender.tab.id, { url: amp.ampurl });
+    } else if (!amp.amplifierOn && amp.isamp && amp.canonicalurl != null) {
+    	chrome.tabs.update(sender.tab.id, { url: amp.canonicalurl });
+    }
 });
 
 //
 chrome.pageAction.onClicked.addListener(function(tab){
   var amp = ampTabs[tab.id];
   if (amp.isamp) {
+	// the current state is amp on - switch it to canonical
     if (amp.canonical != null) {
+      ampTabs[tab.id].amplifierOn = false;
       chrome.tabs.update(tab.id, { url: amp.canonical });
     }   
   } else {
+	// current state is amp off - switch to amp on
+	ampTabs[tab.id].amplifierOn = true;
     chrome.tabs.update(tab.id, { url: amp.ampurl });
   }
 });
