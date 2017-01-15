5ca20ffd6980b6a97e6cb3908e1ae5bdf53bb965
Taylor Raack <taylor@raack.info>
2017-01-15 08:01:15 -0800

    Add artifact creation script

---
+++ compress.sh
@@ -0,0 +1,24 @@
+rm -f pageaccel.zip
+zip pageaccel.zip \
+amplifier128.png \
+amplifier48.png \
+amplifier.js \
+amplify.png \
+background.js \
+canonical.png \
+colors.css \
+firstRun2.html \
+firstRun.js \
+LICENSE \
+manifest.json \
+popup.html \
+popup.js \
+publicsuffixlist.js \
+punycode.js \
+README.md \
+style.css \
+switcher.png \
+chrome/arrow.png \
+chrome/bolt.jpg \
+chrome/bowie_simplified.jpg \
+chrome/bowie_standard.jpg
